4ebbf99a41b81e5a577bc4d900eafb1225d1cfa6
Rahul Janak <d456dev@gmail.com>
2021-04-17 20:49:41 +0300

    Vim: Added reference to surround plugin

--- Vim/install.ps1
+++ Vim/install.ps1
@@ -6,10 +6,8 @@ if ( -Not (Test-Path -Path $VimAutoloadPath )) {
 
 Write-Host "Installing Vim Plug extension..."
 
-$VimPlugGitHubURI = https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
+$VimPlugGitHubURI = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" 
 $VimPlugFileFromWeb = Invoke-WebRequest -UseBasicParsing $VimPlugGitHubURI
 $VimPlugFileFromWeb | New-Item $HOME/vimfiles/autoload/plug.vim -Force
 
 Write-Host "Done."
-
-    
