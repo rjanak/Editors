4bf55497730211a888b7743787b9747a4766d392
Rahul Janak <rhljnk@gmail.com>
2021-04-17 18:17:23 +0300

    Added install script for Vim

---
+++ Vim/install.ps1
@@ -0,0 +1,15 @@
+$VimAutoloadPath = $HOME + "\vimfiles\autoload"
+
+if ( -Not (Test-Path -Path $VimAutoloadPath )) {
+    Write-Host $VimAutoloadPath " doesn't exist, directory will be created"
+}
+
+Write-Host "Installing Vim Plug extension..."
+
+$VimPlugGitHubURI = https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
+$VimPlugFileFromWeb = Invoke-WebRequest -UseBasicParsing $VimPlugGitHubURI
+$VimPlugFileFromWeb | New-Item $HOME/vimfiles/autoload/plug.vim -Force
+
+Write-Host "Done."
+
+    
